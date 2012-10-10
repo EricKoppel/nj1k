3f6dd5a4890700833c3502919186b415f7b8e486
erko <erko@dewire.com>
2012-10-10 12:25:47 +0200

    Added finishers list.

--- nj1k/app/controllers/UsersController.java
+++ nj1k/app/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.AscentEntity;
+import models.FinisherEntity;
 import models.UserEntity;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -8,8 +9,9 @@ import play.mvc.Result;
 public class UsersController extends Controller {
 	
 	public static Result list() {
-		return ok(views.html.users.render(UserEntity.findAll()));
+		return ok(views.html.users.render(FinisherEntity.findFinishers(), UserEntity.findAll()));
 	}
+
 	
 	public static Result showUser(Long id) {
 		return ok(views.html.user.render(UserEntity.findCompleteUser(id), AscentEntity.findByUserId(id)));
