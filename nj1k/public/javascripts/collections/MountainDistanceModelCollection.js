7a3d230afab9d138828722d4349eebdade9d5de5
erko <erko@dewire.com>
2013-08-29 21:09:41 +0200

    removed hardcoded localhost

--- nj1k/public/javascripts/collections/MountainDistanceModelCollection.js
+++ nj1k/public/javascripts/collections/MountainDistanceModelCollection.js
@@ -1,7 +1,7 @@
 var MountainDistanceModelCollection = Backbone.Collection.extend({
 	model: MountainDistanceModel,
 	url: function() {
-		return 'http://localhost:9000/radius/' + this.collectionId;
+		return '/radius/' + this.collectionId;
 	},
 	initialize: function() {
 		console.debug("Initializing MountainDistanceModelCollection");
