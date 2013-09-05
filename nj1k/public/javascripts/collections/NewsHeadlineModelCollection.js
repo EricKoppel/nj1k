27bb7ec9bd90012c15bdf5df552f342da1f852c9
erko <erko@dewire.com>
2013-09-05 22:27:21 +0200

    Just cleaning up a bit

--- nj1k/public/javascripts/collections/NewsHeadlineModelCollection.js
+++
@@ -1,9 +0,0 @@
-var NewsHeadlineModelCollection = Backbone.Collection.extend({
-	model: NewsHeadline,
-	url: function() {
-		return 'http://localhost:9000/news/page/' + this.collectionId;
-	},
-	initialize: function() {
-		console.debug("Initializing NewsHeadlineModelCollection");
-	}
-});
