f4fac95b9dcef5ad923ffa52f01bc14e25179786
erko <erko@dewire.com>
2014-08-19 12:27:39 +0200

    Fixed resize task

--- nj1k/app/Global.java
+++ nj1k/app/Global.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import models.AscentDetailEntity;
 import models.MountainEntity;
@@ -15,8 +18,10 @@ import org.slf4j.LoggerFactory;
 import play.Application;
 import play.GlobalSettings;
 import play.data.format.Formatters;
+import play.mvc.Http.MultipartFormData.FilePart;
 import utils.CustomDateConverter;
 import utils.CustomMountainConverter;
+import utils.ImageResizeTask;
 
 public class Global extends GlobalSettings {
 
@@ -29,70 +34,68 @@ public class Global extends GlobalSettings {
 		registerDateConverter(app);
 		registerMountainConverter(app);
 
-		// loadProfileImages(app);
-		// loadTripReportImages(app);
-		// loadMountainPics(app);
-		// loadNewsImages(app);
+//		try {
+//			loadTripReportImages(app);
+//			loadMountainPics(app);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
-	private void loadNewsImages(Application app) {
-		List<NewsImageEntity> details = NewsImageEntity.find.all();
-
-		for (NewsImageEntity e : details) {
-			if (e.image == null) {
-				String filename = "/public/images/news_images/" + e.caption;
-
-				try {
-					File f = app.getFile(filename);
-					if (f.exists()) {
-						logger.info("Filename: {}", f);
-						e.image = FileUtils.readFileToByteArray(app.getFile(filename));
-						e.save();
-					}
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		}
-	}
-
-	private void loadMountainPics(Application app) {
+	private void loadMountainPics(Application app) throws Exception {
 		List<MountainEntity> details = MountainEntity.find.all();
+		ExecutorService service = Executors.newFixedThreadPool(4);
 
 		for (MountainEntity e : details) {
-			if (e.picture == null) {
-				String filename = "/public/images/mountains/" + e.id + ".jpg";
+			if (e.picture != null) {
 
-				try {
-					File f = app.getFile(filename);
-					if (f.exists()) {
-						logger.info("Filename: {}", f);
-						e.picture = FileUtils.readFileToByteArray(app.getFile(filename));
-						e.save();
+				service.execute(() -> {
+					try {
+						File file = new File("mt" + e.id + ".jpg");
+						FileOutputStream out = new FileOutputStream(file);
+						out.write(e.picture);
+						out.close();
+
+						FilePart part = new FilePart("image", "image.jpg", "image/jpg", file);
+						ImageResizeTask<NewsImageEntity> image = new ImageResizeTask<NewsImageEntity>(part, NewsImageEntity.class);
+
+						e.thumbnail = image.call().thumbnail;
+						e.update();
+
+						file.delete();
+					} catch (Exception e1) {
+						e1.printStackTrace();
 					}
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				});
 			}
 		}
 	}
 
-	private void loadTripReportImages(Application app) {
+	private void loadTripReportImages(Application app) throws Exception {
 		List<AscentDetailEntity> details = AscentDetailEntity.find.all();
+		ExecutorService service = Executors.newFixedThreadPool(4);
 
 		for (AscentDetailEntity e : details) {
-			if (e.descriptor != null) {
-				String filename = "/public/images/tr_images/" + e.descriptor;
-				logger.info("Filename: {}", filename);
-				try {
-					e.image = FileUtils.readFileToByteArray(app.getFile("/public/images/tr_images/" + e.descriptor));
-					e.save();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+
+			if (e.image != null) {
+				service.execute(() -> {
+					try {
+					File file = new File("tr" + e.id + ".jpg");
+					FileOutputStream out = new FileOutputStream(file);
+					out.write(e.image);
+					out.close();
+
+					FilePart part = new FilePart("image", "image.jpg", "image/jpg", file);
+					ImageResizeTask<AscentDetailEntity> image = new ImageResizeTask<AscentDetailEntity>(part, AscentDetailEntity.class);
+
+					e.thumbnail = image.call().thumbnail;
+					e.update();
+
+					file.delete();
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				});
 			}
 		}
 	}
