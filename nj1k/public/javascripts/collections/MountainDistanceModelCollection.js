c40f1f23ce59264397b2303a310c9a669f145b41
erko <erko@dewire.com>
2014-09-20 17:21:35 +0200

    microdata

--- nj1k/public/javascripts/collections/MountainDistanceModelCollection.js
+++
@@ -1,6 +0,0 @@
-var MountainDistanceModelCollection = Backbone.Collection.extend({
-	model: MountainDistanceModel,
-	url: function() {
-		return '/mountains/' + this.collectionId + '/neighbors';
-	}
-});
